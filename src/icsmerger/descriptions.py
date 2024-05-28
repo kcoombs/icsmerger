30edc5163d35bb5ac9b94e90d5f15e80bcfce6a0
kcoombs <github@coombscloud.com>
2024-05-28 11:00:13 -0600

    Fix ICS1 description.

--- src/icsmerger/descriptions.py
+++ src/icsmerger/descriptions.py
@@ -1,6 +1,6 @@
 gui_descriptions = {
     "root_title": "ICS Merger",
-    "ics1_description": "The iCal (.ics) file you used for the previous run of this tool.\n\nThis file will automatically be backed-up.\n\nIf no file, or an invalid file, is provided, an empty iCal with no events will be used. This useful for the first run of this program.",
+    "ics1_description": "The iCal (.ics) file you used for the previous run of this tool.\n\nIf no file, or an invalid file, is provided, an empty iCal with no events will be used. This useful for the first run of this program.",
     "ics2_description": "The new iCal (.ics) file to grab events from. These events will be merged with events from ICS1, if provided.",
     "exclusions_description": "An optional file containing one string per line. When matched to an event's Summary, these strings will cause that event to be excluded from the output",
     "ics1": "ICS1",
