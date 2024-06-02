19373b75a6f743b0919dc7e72d8480e7b21e4a3b
kcoombs <github@coombscloud.com>
2024-06-02 14:30:28 -0600

    Bump version: 0.2.0 → 0.2.1

--- src/icsmerger/__init__.py
+++ src/icsmerger/__init__.py
@@ -1,5 +1,5 @@
 import logging
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 
 # Configure the logging module to output diagnostic information
     # CRITICAL (50): A very serious error that may prevent the program from continuing to run.
